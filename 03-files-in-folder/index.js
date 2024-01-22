82ab00761d19e9a03585e1027076954f39bae524
shamrep <shamrep@gmail.com>
2024-01-22 22:25:30 +0300

    feat: solve task 3

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('node:path');
+const folderPath = path.join(__dirname, 'secret-folder');
+
+fs.readdir(folderPath, (files) => {
+  files.forEach((file) => {
+    const filePath = path.join(folderPath, file);
+    fs.stat(filePath, (stats) => {
+      if (stats.isFile()) {
+        const fileSize = (stats.size / 1024).toFixed(3) + 'kb';
+        const fileName = path.parse(file).name;
+        const fileExtension = path.parse(file).ext.slice(1);
+        console.log(`${fileName} - ${fileExtension} - ${fileSize}`);
+      }
+    });
+  });
+});
