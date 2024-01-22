b99aad68fbc9e42e4b19300b56dfbe5696d92716
shamrep <shamrep@gmail.com>
2024-01-22 22:53:57 +0300

    fix: solve task 3

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -2,10 +2,10 @@ const fs = require('fs');
 const path = require('node:path');
 const folderPath = path.join(__dirname, 'secret-folder');
 
-fs.readdir(folderPath, (files) => {
+fs.readdir(folderPath, (err, files) => {
   files.forEach((file) => {
     const filePath = path.join(folderPath, file);
-    fs.stat(filePath, (stats) => {
+    fs.stat(filePath, (err, stats) => {
       if (stats.isFile()) {
         const fileSize = (stats.size / 1024).toFixed(3) + 'kb';
         const fileName = path.parse(file).name;
